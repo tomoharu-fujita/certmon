197fd4d9c1dae2e5931c1bfc24e8ef688b04b289
Tomoharu Fujita <tomoharu.fujita@connecty.co.jp>
2014-07-08 14:37:50 +0900

    fix for SunCertPathBuilderException

--- src/certmon/core.clj
+++ src/certmon/core.clj
@@ -1,8 +1,8 @@
 (ns certmon.core
-  (:import [javax.net.ssl HttpsURLConnection SSLSocket SSLSocketFactory SSLSession]
-          [java.security.cert X509Certificate]
-          [javax.security.auth.x500 X500Principal]))
-
+  (:import [javax.net.ssl HttpsURLConnection SSLSocket SSLSocketFactory SSLSession SSLContext X509TrustManager TrustManager]
+           [java.security SecureRandom]
+           [java.security.cert X509Certificate]
+           [javax.security.auth.x500 X500Principal]))
 
 (defn- format-certs [^X509Certificate cert]
   (assoc {}
@@ -11,12 +11,24 @@
     :expire  (.. cert getNotAfter toString)
     :since   (.. cert getNotBefore toString)))
 
+;; fix for
+;;   SunCertPathBuilderException:
+;;     unable to find valid certification path to requested target
+(def trust-manager
+  (reify X509TrustManager
+    (checkClientTrusted [_ _ _])
+    (checkServerTrusted [_ _ _])
+    (getAcceptedIssuers [_] nil)))
+
 (defn
   ^{:doc "get server cert information from a specified host"}
   get-cert [^String hostname ^long port]
-  (with-open [^SSLSocket socket (doto
-                                  (. (HttpsURLConnection/getDefaultSSLSocketFactory)
-                                   createSocket
-                                   hostname port) (.startHandshake))]
-   (mapv format-certs (.. socket (getSession) (getPeerCertificates)))
-  ))
+
+  (let [sc (doto (SSLContext/getInstance "SSL")
+             (.init nil
+                    (into-array TrustManager [trust-manager])
+                    (SecureRandom.)))]
+    (with-open [^SSLSocket socket (doto (. (.getSocketFactory sc) createSocket hostname port)
+                                    (.startHandshake))]
+      (mapv format-certs (.. socket (getSession) (getPeerCertificates)))
+      )))
